7f6be79672b0a3073e5051f029e47142fc16a50a
lmt <1571min@naver.com>
2020-04-09 09:35:39 +0900

    update server.js

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,3 +1,10 @@
+/* 
+  README.md 파일을 꼭 읽으시길 바랍니다. 모든 basic requirements가 정리되어 있습니다.  
+  HINT : 
+    request 의 method 와 url을 활용하여 클라이언트의 요청을 분기 할 수 있어야 합니다.
+    잘못된 요청은 잘못된 요청이라는 응답을 주어야 합니다.
+  */
+// Readable streams emit 'data' events once a listener is added.
 const http = require('http');
 
 const PORT = 5000;
@@ -6,17 +13,30 @@ const ip = 'localhost';
 
 const server = http.createServer((request, response) => {
   let headers = defaultCorsHeader;
-  /* 
-  README.md 파일을 꼭 읽으시길 바랍니다. 모든 basic requirements가 정리되어 있습니다.  
-  HINT : 
-    request 의 method 와 url을 활용하여 클라이언트의 요청을 분기 할 수 있어야 합니다.
-    잘못된 요청은 잘못된 요청이라는 응답을 주어야 합니다.
-  */
-  console.log(
-    `http request method is ${request.method}, url is ${request.url}`
-  );
-  response.writeHead(200, headers);
-  response.end('hello mini-server sprints');
+  if (request.method === 'OPTIONS') {
+    console.log('this is options');
+    response.writeHead(200, headers);
+    response.end();
+  } else if (request.method === 'POST') {
+    console.log('this is post');
+    let body = '';
+    response.writeHead(200, headers);
+    request.on('data', (chunk) => {
+      body += chunk.toString(); // convert Buffer to string
+    });
+    request.on('end', () => {
+      if (request.url === '/upper') {
+        response.end(body.toUpperCase());
+      } else if (request.url === '/lower') {
+        response.end(body.toLowerCase());
+      } else {
+        response.end(body);
+      }
+    });
+    console.log(
+      `http request method is ${request.method}, url is ${request.url}`
+    );
+  }
 });
 
 server.listen(PORT, ip, () => {
@@ -27,5 +47,5 @@ const defaultCorsHeader = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age': 10
+  'access-control-max-age': 10,
 };
